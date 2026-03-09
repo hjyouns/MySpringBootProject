39ffbb89d3924e2c2b87bcc74d1633a648675031
hjyouns <hjyouns@gmail.com>
2026-03-09 10:03:10 +0900

    Prod config update

--- src/main/java/com/basic/myspringboot/config/ProdConfig.java
+++ src/main/java/com/basic/myspringboot/config/ProdConfig.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
+@Profile("prod")
 
-public class TestConfig {
+public class ProdConfig {
     @Bean
     public CustomerVO customerVO() {
         return CustomerVO.builder() //CustomerVOBuilder
-                .mode("테스트 환경")
-                .rate(0.5)
+                .mode("운영 환경")
+                .rate(1.5)
                 .build();
     }
 }
