c69893c81ce312f90d4b26d3b48a9596ab6f04d6
hjyouns <hjyouns@gmail.com>
2026-03-10 10:10:52 +0900

    CustomerTest update

--- src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -1,7 +1,30 @@
 package com.basic.myspringboot.repository;
 
+import com.basic.myspringboot.entity.Customer;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CustomerRepositoryTest {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    //1. Customer 등록
+    @Test
+    void testCreate() {
+        //Given(준비단계)
+        Customer customer = new Customer();
+        customer.setCustomerId("A002");
+        customer.setCustomerName("스프링부트");
+        //When(실행단계)
+        Customer addCustomer = customerRepository.save(customer);
+        //Then(검증단계)
+        assertThat();
+    }
 
 }
