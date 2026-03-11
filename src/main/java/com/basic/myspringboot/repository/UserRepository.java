66750c9545d757f238e80dcda70dcc3bd1da1f81
hjyouns <hjyouns@gmail.com>
2026-03-11 09:33:18 +0900

    BusinessException add

--- src/main/java/com/basic/myspringboot/repository/UserRepository.java
+++ src/main/java/com/basic/myspringboot/repository/UserRepository.java
@@ -1,12 +1,32 @@
-package com.basic.myspringboot.repository;
+package com.basic.myspringboot.controller;
 
 import com.basic.myspringboot.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.basic.myspringboot.repository.CustomerRepository;
+import com.basic.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserRestController {
+    private final UserRepository userRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
-    List<User> findByNameContains(String name);
-    Optional<User> findBy
+    //Constructor Injection
+//    public UserRestController(UserRepository userRepository) {
+//        log.info("UserRepository 구현클래스명 = {}",userRepository.getClass().getName());
+//        this.userRepository = userRepository;
+//    }
+
+    //User 등록
+    @PostMapping
+    public User create(@RequestBody User userDetail) {
+        return userRepository.save(userDetail);
+    }
 }
